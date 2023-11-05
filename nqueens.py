be8c331c2dd7d8ff76a99af5e7b6182c4467cda6
Ishan Kshirsagar <83628182+ishan-kshirsagar0-7@users.noreply.github.com>
2023-11-05 18:32:56 +0530

    Update nqueens.py

--- nqueens.py
+++ nqueens.py
@@ -1,52 +1,62 @@
-from copy import deepcopy
+class NQBacktracking:
+    def __init__(self, N, x_, y_):
+        self.N = N
+        self.x = x_
+        self.y = y_
+        self.ld = [0] * (2 * N - 1)
+        self.rd = [0] * (2 * N - 1)
+        self.cl = [0] * N
+        self.solutions = []
 
-# Check if a queen can be placed on board[row][col]
-def is_safe(board, row, col, n):
-    # Check this row on the left side
-    for i in range(col):
-        if board[row][i]:
-            return False
+    def printSolutions(self):
+        for idx, solution in enumerate(self.solutions, 1):
+            print(f"Solution Number {idx}: ")
+            for i in range(self.N):
+                for j in range(self.N):
+                    if solution[i][j] == 1:
+                        print('Q', end=' ')
+                    else:
+                        print('_', end=' ')
+                print()
+            print()
+        print(f"Total {len(self.solutions)} solutions found.")
     
-    # Check upper diagonal on the left side
-    for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
+    def solveNQUtil(self, board, col):
+        if col >= self.N:
+            # Copy the board to the list of solutions
+            self.solutions.append([row[:] for row in board])
+            return False  # Change this line to search for all solutions
+        if col == self.y:
+            return self.solveNQUtil(board, col + 1)
+        for i in range(self.N):
+            if i == self.x:
+                continue
+            if (self.ld[i - col + self.N - 1] != 1 and self.rd[i + col] != 1) and self.cl[i] != 1:
+                board[i][col] = 1
+                self.ld[i - col + self.N - 1] = self.rd[i + col] = self.cl[i] = 1
+                # Continue searching for more solutions
+                self.solveNQUtil(board, col + 1)
+                # Backtrack
+                board[i][col] = 0
+                self.ld[i - col + self.N - 1] = self.rd[i + col] = self.cl[i] = 0
+        return False
     
-    # Check lower diagonal on the left side
-    for i, j in zip(range(row, n, 1), range(col, -1, -1)):
-        if board[i][j]:
-            return False
-    
-    return True
+    def solveNQ(self):
+        board = [[0 for _ in range(self.N)] for _ in range(self.N)]
+        board[self.x][self.y] = 1
+        self.ld[self.x - self.y + self.N - 1] = self.rd[self.x + self.y] = self.cl[self.x] = 1
+        self.solveNQUtil(board, 0)
+        if not self.solutions:
+            print("No solution exists")
+        else:
+            self.printSolutions()
 
-# Main recursive function to solve N-Queens
-def solve_n_queens_util(board, col, n):
-    # Base case: If all queens are placed
-    if col >= n:
-        return [deepcopy(board)]
-    
-    solutions = []
-    # Try placing queen in all rows one by one
-    for i in range(n):
-        if is_safe(board, i, col, n):
-            board[i][col] = 1
-            for solution in solve_n_queens_util(board, col + 1, n):
-                solutions.append(deepcopy(solution))
-            board[i][col] = 0  # backtrack
-    
-    return solutions
+# Parameters for the N-Queens problem
+N = int(input("Enter size of board: "))  # Size of the chessboard
+x = int(input("Enter the row of initial position of first queen: "))  # Row for the initial position of the 1st queen
+y = int(input("Enter the column of initial position of first queen: "))  # Column for the initial position of the 1st queen
 
-# Driver code
-if __name__ == "__main__":
-    n = 4  # Number of queens and size of board (n x n)
-    board = [[0 for _ in range(n)] for _ in range(n)]
-    
-    # Solve and get all possible solutions
-    solutions = solve_n_queens_util(board, 0, n)
-    
-    # Print solutions
-    for idx, solution in enumerate(solutions):
-        print(f"Solution {idx + 1}:")
-        for row in solution:
-            print(" ".join(str(cell) for cell in row))
-        print()
+# Initialize the NQBacktracking class with the parameters
+NQBt = NQBacktracking(N, x, y)
+# Execute the solveNQ method to find and print all solutions
+NQBt.solveNQ()
